4abbd599416286010507fb1650366c3ba117bc35
Grant Collins <grantcollins03@gmail.com>
2025-06-13 08:38:57 -0400

    install dependences; setup server

---
+++ app.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res, next) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
